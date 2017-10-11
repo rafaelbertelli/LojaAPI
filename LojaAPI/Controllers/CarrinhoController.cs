7e17c251b5fba59fb99fb0cae6670563218c5fe4
Rafael Bertelli <rafaelbertelli89@gmail.com>
2017-10-10 23:11:27 -0300

    Get & Post

---
+++ LojaAPI/Controllers/CarrinhoController.cs
@@ -0,0 +1,41 @@
+﻿using Loja.DAO;
+using Loja.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LojaAPI.Controllers
+{
+    public class CarrinhoController : ApiController
+    {
+        public Carrinho Get(int id)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            Carrinho carrinho = dao.Busca(id);
+
+            return carrinho;
+        }
+
+        // forca ser o retorno em XML
+        //public string Get(int id)
+        //{
+        //    CarrinhoDAO dao = new CarrinhoDAO();
+        //    Carrinho carrinho = dao.Busca(id);
+
+        //    return carrinho.ToXml();
+        //}
+
+        public string Post([FromBody]Carrinho carrinho)
+        {
+            CarrinhoDAO dao = new CarrinhoDAO();
+            dao.Adiciona(carrinho);
+            return "Sucesso!";
+        }
+
+
+
+    }
+}
